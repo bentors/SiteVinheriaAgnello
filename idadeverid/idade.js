4ee27a948bcbd36bdef0919a9e836effeb5836c6
bentors <bento.rangel05@gmail.com>
2024-04-25 20:58:43 -0300

    switch exemplo
    comentado no idadeverif/idade tem exemplo de switchcase

--- idadeverid/idade.js
+++ idadeverid/idade.js
@@ -1,8 +1,24 @@
 function sim(event) 
 {
-window.location.href = 'https://google.com';
+window.location.href = '../site/index.html';
 }
 
 function não(event) {
 window.location.href = "https://www.peppapig.com/pt-br";
 }
+
+/* 
+var vinhos = "jngfo"
+
+switch (vinhos){
+    case "rose":
+        console.log("Vai bem com macarrão a bolonhesa")
+    break;
+    case "branco":
+        console.log("Vai bem com risoto");
+    break;
+    default:
+        console.log("Infelizmente não temos essa opção")
+    break
+
+} */
